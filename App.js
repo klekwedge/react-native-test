d6ccb7470d823f6599d22a020a1e440aacaa5686
Andrey <antonov.andrey.10101@gmail.com>
2023-03-25 11:47:20 +0300

    feat: transfer article display code to HomeScreen

--- App.js
+++ App.js
@@ -1,65 +1,11 @@
-import axios from "axios";
 import React from "react";
-import { RefreshControl } from "react-native";
-import {
-  StatusBar,
-  Alert,
-  View,
-  Text,
-  FlatList,
-  ActivityIndicator,
-  TouchableOpacity,
-} from "react-native";
-import Post from "./components/Post";
+import { StatusBar, View } from "react-native";
+import HomeScreen from "./screens/Home";
 
 export default function App() {
-  const [isLoading, setIsLoading] = React.useState(false);
-  const [items, setItems] = React.useState();
-
-  const fetchPosts = () => {
-    setIsLoading(true);
-    axios
-      .get("https://637283b2025414c63711bbb5.mockapi.io/articles")
-      .then(({ data }) => {
-        setItems(data);
-      })
-      .catch((err) => {
-        console.log(err);
-        Alert.alert("Ошибка", "Не удалось получить статьи");
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  };
-
-  React.useEffect(fetchPosts, []);
-
-  if (isLoading) {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <ActivityIndicator size="large" />
-        <Text style={{ marginTop: 15 }}>Загрузка...</Text>
-      </View>
-    );
-  }
-
   return (
     <View>
-      <FlatList
-        refreshControl={
-          <RefreshControl refreshing={isLoading} onRefresh={fetchPosts} />
-        }
-        data={items}
-        renderItem={({ item }) => (
-          <TouchableOpacity >
-            <Post
-              title={item.title}
-              createdAt={item.createdAt}
-              imageUrl={item.imageUrl}
-            />
-          </TouchableOpacity>
-        )}
-      />
+      <HomeScreen />
       <StatusBar theme="auto" />
     </View>
   );
---
+++ screens/Home.jsx
@@ -0,0 +1,64 @@
+import axios from "axios";
+import React from "react";
+import {
+  Alert,
+  RefreshControl,
+  View,
+  Text,
+  FlatList,
+  ActivityIndicator,
+  TouchableOpacity,
+} from "react-native";
+import Post from "../components/Post";
+
+export default function HomeScreen() {
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [items, setItems] = React.useState();
+
+  const fetchPosts = () => {
+    setIsLoading(true);
+    axios
+      .get("https://637283b2025414c63711bbb5.mockapi.io/articles")
+      .then(({ data }) => {
+        setItems(data);
+      })
+      .catch((err) => {
+        console.log(err);
+        Alert.alert("Ошибка", "Не удалось получить статьи");
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  React.useEffect(fetchPosts, []);
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+        <Text style={{ marginTop: 15 }}>Загрузка...</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View>
+      <FlatList
+        refreshControl={
+          <RefreshControl refreshing={isLoading} onRefresh={fetchPosts} />
+        }
+        data={items}
+        renderItem={({ item }) => (
+          <TouchableOpacity>
+            <Post
+              title={item.title}
+              createdAt={item.createdAt}
+              imageUrl={item.imageUrl}
+            />
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
+}
