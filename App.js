25515b2fad525b49411e79b6e6012f02c4fe3656
Andrey <antonov.andrey.10101@gmail.com>
2023-03-25 11:40:31 +0300

    RefreshController

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React from "react";
+import { RefreshControl } from "react-native";
 import {
   StatusBar,
   Alert,
@@ -44,6 +45,9 @@ export default function App() {
   return (
     <View>
       <FlatList
+        refreshControl={
+          <RefreshControl refreshing={isLoading} onRefresh={fetchPosts} />
+        }
         data={items}
         renderItem={({ item }) => (
           <Post
